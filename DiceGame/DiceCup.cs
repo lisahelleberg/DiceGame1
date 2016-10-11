3e81438fbbd56bc4e7343c116aac2674a01e15df
Bruger <Bruger@W7-THINKPAD>
2016-10-11 10:12:26 +0200

    Add project files.

---
+++ DiceGame/DiceCup.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DiceGame
+{
+    class DiceCup
+    {
+        private Die die1red; // Instance fields of the Die-type (The Die class). This way it keeps the methods from the Die-class
+        private Die die2;
+        private Die die3;
+        
+
+        public DiceCup()
+        {
+            // VERY IMPORTANT! When we have instance fields of a class type,
+            // we MUST remember to create actual objects in the constructor,
+            // that the instance fields can then point to.
+            die1red = new Die();
+            die2 = new Die();
+            die3 = new Die();
+            
+        }
+        /// <summary>
+        /// Rolls all dice in the cup
+        /// </summary>
+
+        // You must create a method with the below header:
+        public void RollAllDiceInCup()
+        {
+            //die1.RollDie();
+            //die2.RollDie();
+            RollTwoDice();
+            die3.RollDie();
+            
+        }
+
+        public void RollTwoDice()
+        {
+            die1red.RollDie();
+            die2.RollDie();
+        }
+
+        public void RollRedDie()
+        {
+
+        }
+        // You must create a method with the below header:
+        public int GetTotalValue()
+        {
+            
+            
+        }
+
+        // You must create a method with the below header:
+       // public bool IsTotalValueLargerThan(int value)
+       // {
+        
+       // }
+    }
+}
---
+++ DiceGame/InsertCodeHere.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DiceGame
+{
+    class InsertCodeHere
+    {
+        public void MyCode()
+        {
+            // The FIRST line of code should be BELOW this line
+
+            // New die
+            Die die1 = new Die();
+            die1.RollDie();
+            Console.WriteLine("The value of the dice is {0}", die1.GetValue());
+
+            Die die2 = new Die();
+            die2.RollDie();
+            Console.WriteLine("The value of the dice is {0}", die2.GetValue());
+
+            Die die3 = new Die();
+            die2.RollDie();
+            Console.WriteLine("The value of the dice is {0}", die3.GetValue());
+
+
+            // The LAST line of code should be ABOVE this line
+        }
+    }
+}
